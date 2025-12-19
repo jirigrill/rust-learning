90a176c1c1c32308262a64f8034f47110da2f17f
Jiri Grill <grilljiri@gmail.com>
2025-12-19 12:54:19 +0100

    finished rustlings: 13_error_handling

--- rustlings/exercises/13_error_handling/errors2.rs
+++ rustlings/exercises/13_error_handling/errors2.rs
@@ -21,7 +21,17 @@ fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let cost_per_item = 5;
 
     // TODO: Handle the error case as described above.
-    let qty = item_quantity.parse::<i32>();
+    let qty = item_quantity.parse::<i32>()?;
+
+    // let parsed_qty = item_quantity.parse::<i32>();
+    // let qty = match parsed_qty {
+    //     Ok(value) => value,
+    //     Err(error) => return Err(error),
+    // };
+
+    // let qty = item_quantity
+    //     .parse::<i32>()
+    //     .expect("item_quantity has to be number");
 
     Ok(qty * cost_per_item + processing_fee)
 }
